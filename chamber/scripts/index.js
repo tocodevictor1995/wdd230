66a7c98f8bef6585ab1d8d8928110d3e8fd00256
tocodevictor1995 <132182198+tocodevictor1995@users.noreply.github.com>
2024-07-14 02:29:06 +0100

    Add files via upload

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,12 +1,9 @@
-// index.js
+// scripts/common.js
 
-// Set the current year in the footer
 document.getElementById('year').textContent = new Date().getFullYear();
 
-// Display the last modified date in the footer
 document.getElementById('lastmodified').textContent = `Last modified: ${document.lastModified}`;
 
-// Hamburger menu button functionality
 const hbutton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
@@ -15,9 +12,9 @@ hbutton.addEventListener('click', () => {
     hbutton.classList.toggle('open');
 });
 
-// Dark mode toggle button functionality
 const darkModeBtn = document.getElementById('dark-mode-btn');
 const body = document.body;
+const welcomeText = document.querySelector('#welcome');
 
 darkModeBtn.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
@@ -25,7 +22,34 @@ darkModeBtn.addEventListener('click', () => {
     localStorage.setItem('darkMode', darkModeEnabled);
 });
 
-// Check dark mode preference on page load
 if (localStorage.getItem('darkMode') === 'true') {
     body.classList.add('dark-mode');
+}
+
+let lastVisitDate = getVisitDate() || 0;
+
+if (lastVisitDate !== 0) {
+    let oldDate = new Date(lastVisitDate);
+    let diff = (Date.now() - oldDate.getTime()) / (1000 * 60 * 60 * 24); // difference in days
+    if (diff < 1) {
+        welcomeText.textContent = 'Back so soon! Awesome!';
+        lastVisitDate = Date.now();
+        setVisitDate(lastVisitDate);
+    } else {
+        welcomeText.textContent = `You last visited ${Math.floor(diff)} day(s) ago.`;
+        lastVisitDate = Date.now();
+        setVisitDate(lastVisitDate);
+    }
+} else {
+    welcomeText.textContent = 'Let us know if you have any questions!';
+    lastVisitDate = Date.now();
+    setVisitDate(lastVisitDate);
+}
+
+function getVisitDate() {
+    return Number(JSON.parse(localStorage.getItem('visit-date')));
+}
+
+function setVisitDate(date) {
+    localStorage.setItem('visit-date', JSON.stringify(date));
 }
